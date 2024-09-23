14da6842348958e8c48be3725485a347a480110f
alex <alexandre.goumain@3wa.io>
2024-09-23 14:21:35 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import User from "./User";
+function App() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <User />
+        </>
+    );
+}
+
+export default App;
---
+++ src/User.jsx
@@ -0,0 +1,12 @@
+export default function User() {
+    const prenom = "Alexandre";
+    const nom = "Goumain";
+    return (
+        <>
+            <h2>
+                Helo my name is : {prenom} {nom}
+            </h2>
+            <p>Voici ma belle description</p>
+        </>
+    );
+}
