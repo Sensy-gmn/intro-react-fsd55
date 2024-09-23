8dba02fbf60c122316ef0f03c316171199cecae9
alex <alexandre.goumain@3wa.io>
2024-09-23 15:31:22 +0200

    JS expression added to the User component

--- src/User.jsx
+++ src/User.jsx
@@ -1,12 +1,27 @@
 export default function User() {
     const prenom = "Alexandre";
     const nom = "Goumain";
+    const age = 23;
+
     return (
         <>
-            <h2>
-                Helo my name is : {prenom} {nom}
-            </h2>
-            <p>Voici ma belle description</p>
+            {age >= 18 ? (
+                <>
+                    <h1>
+                        Helo my name is : {prenom} {nom} and i&apos;m {age}
+                        years old
+                    </h1>
+                    <p>Voici ma belle description</p>
+                </>
+            ) : (
+                <>
+                    <h1>
+                        Helo my name is : {prenom} {nom} and i&apos;m {age}
+                        years old
+                    </h1>
+                    <p>Trop jeune mince !</p>
+                </>
+            )}
         </>
     );
 }
